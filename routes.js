606d4dd4cb3aceffee4d408336afcee1d41f261d
Devinan <devinan.hr@gmail.com>
2020-02-26 17:13:33 +0900

    Feature 02 backend (#13)
    * F-2: Initial Middlewares
    
    * F-2: Initial Middlewares (#8)
    
    * Feature b 01 routes (#10)
    
    * F-2: Initial Middlewares (#7)
    
    * F-B-01:Create Global Routes
    
    * F-B-01: Create User Routes
    
    * F-B-01: Create Settings Routers
    
    * F-B-01: Create Help Routes
    
    * F-B-01: Create Books Routers
    
    * F-B-01: Create BookMark Routes
    
    * F-B-01: Export default Routes and Fixed Name Intereset --> Interest
    
    * F-02: Handling Routes andFile Name Change Routes.js --> routes.js
    
    * Delete Routes.js
    
    * File Name Change

---
+++ routes.js
@@ -0,0 +1,68 @@
+// Global
+
+const START = "/start";
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+// User
+
+const USERS = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+// Settings
+
+const SETTINGS = "/settings";
+const INTEREST = "/interest";
+const NOTIFICATION = "/notification";
+
+// Help
+
+const REQUSETS = "/requests";
+const UPLOAD_REQUEST = "/upload";
+const REQUSET_DETAIL = "/:id";
+const NEXT_REQUSET = "/:id/next";
+const FINISH_REQUEST = "/:id/finish";
+
+// Books
+
+const BOOKS = "/books";
+const BOOK_DETAIL = "/:id";
+
+// Bookmark
+
+const BOOKMARKS = "/bookmarks";
+const BOOKMARK_DETAIL = "/:id";
+const DELETE_BOOKMARK = "/:id/delete";
+
+const routes = {
+  start: START,
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  settings: SETTINGS,
+  interest: INTEREST,
+  notification: NOTIFICATION,
+  requests: REQUSETS,
+  uploadRequest: UPLOAD_REQUEST,
+  requestDetail: REQUSET_DETAIL,
+  nextRequest: NEXT_REQUSET,
+  finishRequest: FINISH_REQUEST,
+  books: BOOKS,
+  bookDetail: BOOK_DETAIL,
+  bookmarks: BOOKMARKS,
+  bookmarkDetail: BOOKMARK_DETAIL,
+  deleteBookmark: DELETE_BOOKMARK
+};
+
+export default routes;
