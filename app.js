1fda4b77ee371944a23fbece39c84c56d8c2cd3f
Devinan <devinan.hr@gmail.com>
2020-02-26 21:51:47 +0900

    F-B-02-04: Handle BookRouter (#23)

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import routes from "./routes";
+import bookRouter from "./routers/bookRouter";
 import helpRouter from "./routers/helpRouter";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
@@ -16,6 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+app.use(routes.books, bookRouter);
 app.use(routes.help, helpRouter);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
