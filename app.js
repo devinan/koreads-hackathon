b8ec633c5a1ff51c2222f489cb58831b84311780
Devinan <devinan.hr@gmail.com>
2020-02-26 20:44:57 +0900

    F-B-02-04: Handle BookRouter

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import routes from "./routes";
+import bookRouter from "./routers/bookRouter";
 
 const app = express();
 
@@ -13,28 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
-app.get(routes.start, (req, res) => res.send("Start"));
-app.get(routes.home, (req, res) => res.send("Home"));
-app.get(routes.join, (req, res) => res.send("Join"));
-app.get(routes.login, (req, res) => res.send("Login"));
-app.get(routes.logout, (req, res) => res.send("Logout"));
-app.get(routes.search, (req, res) => res.send("Search"));
-// users;
-// userDetail;
-// editProfile;
-// changePassword;
-// settings;
-// interest;
-// notification;
-// requests;
-// uploadRequest;
-// requestDetail;
-// nextRequest;
-// finishRequest;
-// books;
-// bookDetail;
-// bookmarks;
-// bookmarkDetail;
-// deleteBookmark;
+app.use(routes.books, bookRouter);
 
 export default app;
