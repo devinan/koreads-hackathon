d4fbfdc0aa7f739a217ee93f79b2d9ddff92fce8
Devinan <devinan.hr@gmail.com>
2020-02-27 14:55:21 +0900

    F-B-03: Handling Five Controllers

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,2 +1,3 @@
 export const settings = (req, res) => res.send("Settings");
 export const notification = (req, res) => res.send("Notification");
+export const help = (req, res) => res.send("Help");
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,3 +2,7 @@ export const join = (req, res) => res.send("Join");
 export const interest = (req, res) => res.send("Interest");
 export const login = (req, res) => res.send("LogIn");
 export const logout = (req, res) => res.send("LogOut");
+export const users = (req, res) => res.send("Users");
+export const userDetail = (req, res) => res.send("User Detail");
+export const editProfile = (req, res) => res.send("Edit Profile");
+export const changePassword = (req, res) => res.send("Change Password");
