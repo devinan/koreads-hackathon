6d239e67fb07c0feca5d38beb731e8f5ebdb8e86
Devinan <devinan.hr@gmail.com>
2020-02-27 16:16:48 +0900

    Delete Help Router and Renamed RequestRouter.js

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -4,11 +4,13 @@ import routes from "../routes";
 const globalRouter = express.Router();
 
 globalRouter.get(routes.home);
+globalRouter.get(routes.help);
 globalRouter.get(routes.join);
 globalRouter.get(routes.interest);
 globalRouter.get(routes.login);
 globalRouter.get(routes.logout);
 globalRouter.get(routes.notification);
+globalRouter.get(routes.requests);
 globalRouter.get(routes.search);
 globalRouter.get(routes.settings);
 globalRouter.get(routes.start);
--- routers/requestRouter.js
+++ routers/requestRouter.js
@@ -1,9 +1,8 @@
 import express from "express";
-import routes from "../../routes";
+import routes from "../routes";
 
 const requestRouter = express.Router();
 
-requestRouter.get(routes.home);
 requestRouter.get(routes.requestDetail);
 requestRouter.get(routes.uploadRequest);
 requestRouter.get(routes.nextRequest);
