ac5afc138258b48a90ffbdbc28525401bf01d95e
Devinan <devinan.hr@gmail.com>
2020-02-28 17:50:32 +0900

    Create Layout File

--- controllers/settingController.js
+++ controllers/settingController.js
@@ -1,3 +1,3 @@
 export const settings = (req, res) => res.render("settings");
-export const notification = (req, res) => res.render("notification");
+export const notification = (req, res) => res.send("notification");
 export const help = (req, res) => res.render("help");
