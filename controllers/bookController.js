ad7b882ef7ae15907f157fd437857216e3e6aa06
Devinan <devinan.hr@gmail.com>
2020-02-28 15:52:17 +0900

    Create BookController root

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,3 +1,4 @@
+export const root = (req, res) => res.send("Root");
 export const start = (req, res) => res.send("START");
 export const home = (req, res) => res.send("Home");
 export const search = (req, res) => res.send("Search");
